f3ff9dfefbb46bb30b3ad93555d4aef5d28a0a67
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-06-17 21:46:37 -0700

    coors update

--- app/controllers/pollinators_controller.rb
+++ app/controllers/pollinators_controller.rb
@@ -4,4 +4,25 @@ class PollinatorsController < ApplicationController
         render json: pollinators, except: [:created_at, :updated_at], include: [:plants, :plant_pollinators]
         
     end
+    def show
+        pollinator = Pollinator.find_by(id: params[:id])
+        render json: pollinator
+    end
+    def update
+        pollinator = Pollinator.find(params[:id])
+        pollinator.update!(pollinator_params)
+        if pollinator.valid?
+            render json: pollinator
+        else   
+            render json: pollinator.errors.full_messages
+        end
+    end
+
+    private
+    def pollinator_params
+        params.require(:pollinator).permit(:name, :species, :description, :image)
+        
+    end
 end
+
+
